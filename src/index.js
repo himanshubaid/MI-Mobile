a9a10dc5acaae3cf77b49f6043fb60dbc78f613f
himanshubaid <108008314+himanshubaid@users.noreply.github.com>
2023-01-07 20:48:33 +0530

    final project deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import App from './App';//idhar import kar liya
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    
+  </React.StrictMode>,
+  document.getElementById('root')//render kar ke sara element app se import kar ke iss element mene dal dega 
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
